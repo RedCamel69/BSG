bf136f00bb92beb7935252e2911ec09476283b63
RedCamel69 <RichardJGilbert@hotmail.com>
2016-07-12 11:32:49 +0100

    Add project files.

---
+++ BSG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BSG.Startup))]
+namespace BSG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
